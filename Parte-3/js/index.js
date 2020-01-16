699018bda4434fa6f20b09781251d7f1b9f37c43
Victor <sdkvictor@github.com>
2020-01-16 08:46:11 -0600

    parte 3

---
+++ Parte-3/js/index.js
@@ -0,0 +1,24 @@
+let form = document.getElementById("formComment");
+let commentSection = document.getElementsByClassName("seccionComments");
+
+let names = document.getElementsByClassName("userName");
+names[0].value="";
+
+form.addEventListener("submit",(event)=>{
+    console.log("submit");
+    event.preventDefault();
+    let names = document.getElementsByClassName("userName");
+    console.log(name.value)
+    name = names[0];
+    let comment = document.getElementById("userComment");
+    if(name.value!=""&&comment!=""){
+        let newComment = document.createElement("p");
+        let newName = document.createElement("h3");
+        let newContent = document.createElement("p");
+
+        newComment.innerHTML+="<p><h3>" + name.value + "</h3></p><p>" + comment.value + "</p>";
+        newComment.appendChild(newContent);
+
+        commentSection[0].appendChild(newComment);
+    }
+});
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 #Examen-Parcial-1
+
+git clone https://github.com/sdkvictor/Examen-Parcial-1.git
+echo "#Examen-Parcial-1" >> README.md
+git pull
+git add .
+git commit -m "parte 1 y 2"
+git push
